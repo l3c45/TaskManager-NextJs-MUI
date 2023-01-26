83fc72f9f98157308fc658547a408a62de70a590
Lucas <mrlucaszarate@gmail.com>
2023-01-26 13:33:47 -0300

    Añadir animacion a tarjeta de borrar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets:['latin'] });
 
 export default function Home() {
   return (
-    <main style={{ padding: "1rem",display:"flex",alignItems:"center",flexDirection:"column" }} className={`${inter.className}`}>
+    <main style={{ padding: "1rem",display:"flex",alignItems:"center",flexDirection:"column",position:"relative" }} className={`${inter.className}`}>
      
       <Entries></Entries>
      
--- src/components/ui/DeleteZone.tsx
+++ src/components/ui/DeleteZone.tsx
@@ -1,15 +1,15 @@
-import { Box, useTheme } from "@mui/material";
+import { Box, Grow, Slide, useTheme } from "@mui/material";
 import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
 import { DragEvent, useContext } from "react";
 import { EntriesContext } from "@/context/entries";
 
 const DeleteZone = () => {
   const { palette: theme } = useTheme();
-  const {removeEntrie } = useContext(EntriesContext);
+  const { removeEntrie } = useContext(EntriesContext);
 
   const onDrop = (e: DragEvent) => {
     const id = e.dataTransfer.getData("id");
-    removeEntrie(id)
+    removeEntrie(id);
   };
 
   const allow = (e: DragEvent) => {
@@ -17,14 +17,22 @@ const DeleteZone = () => {
   };
 
   return (
-    <Box
-      onDrop={onDrop}
-      onDragOver={allow}
-      bgcolor={theme.error.main}
-      sx={{ width: "300px", textAlign: "center", padding: 3, marginTop: 2 }}
-    >
-      <DeleteOutlineOutlinedIcon></DeleteOutlineOutlinedIcon>
-    </Box>
+    <Slide direction="up" in={true} mountOnEnter unmountOnExit>
+      <Box
+        onDrop={onDrop}
+        onDragOver={allow}
+        bgcolor={theme.error.main}
+        sx={{
+          width: "300px",
+          textAlign: "center",
+          padding: 2,
+          position: "fixed",
+          bottom: 0,
+        }}
+      >
+        <DeleteOutlineOutlinedIcon></DeleteOutlineOutlinedIcon>
+      </Box>
+    </Slide>
   );
 };
 
