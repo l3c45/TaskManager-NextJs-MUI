da40575dca5caa2bd11c383a697aec24b7407f64
Lucas <mrlucaszarate@gmail.com>
2023-01-25 22:44:09 -0300

    IMplementar PWA

--- next.config.js
+++ next.config.js
@@ -1,11 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+//const runtimeCaching = require("next-pwa/cache");
+const withPWA = require("next-pwa")({
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    buildExcludes: [/middleware-manifest.json$/],
+});
+
+const nextConfig = withPWA({
   experimental: {
     fontLoaders: [
       { loader: '@next/font/google', options: { subsets: ['latin'] } },
     ],
     appDir: true,
   },
-}
-
-module.exports = nextConfig
+});
+module.exports = nextConfig;
--- src/app/head.tsx
+++ src/app/head.tsx
@@ -2,10 +2,22 @@ export default function Head() {
   return (
     <>
       <title>Task Manager</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
+
       <meta name="description" content="Administra tares de forma sencilla" />
       <link rel="icon" href="/favicon-16x16.png" />
-  
+
+      <meta charSet="utf-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta
+        name="viewport"
+        content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+      />
+
+      <meta name="theme-color" content="#000" />
+
+      <link rel="manifest" href="/manifest.json" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" href="/apple-icon.png"></link>
     </>
-  )
+  );
 }
